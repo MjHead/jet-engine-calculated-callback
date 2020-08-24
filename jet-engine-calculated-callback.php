90e6bd7be6a464ac8fad1ac25e72d588c879f5a4
mjhead <andremjhead@gmail.com>
2020-08-24 11:24:48 +0300

    ADD: process calculations

--- jet-engine-calculated-callback.php
+++ jet-engine-calculated-callback.php
@@ -18,11 +18,69 @@ if ( ! defined( 'WPINC' ) ) {
 
 class Jet_Engine_Calculated_Callback_Addon {
 
+	/**
+	 * Instance.
+	 *
+	 * Holds the plugin instance.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 *
+	 * @var Plugin
+	 */
+	public static $instance = null;
+
 	public $config = array();
 
 	public function __construct() {
-		add_filter( 'jet-engine/listings/allowed-callbacks', 'jet_add_calc_callback', 10, 2 );
+		add_filter( 'jet-engine/listings/allowed-callbacks', array( $this, 'add_calc_callback' ), 10, 2 );
 		add_filter( 'jet-engine/listing/dynamic-field/callback-args', array( $this, 'add_field_to_args' ), 10, 3 );
+		add_action( 'jet-engine/listing/dynamic-field/callback-controls', array( $this, 'callback_controls' ) );
+	}
+
+	public function callback_controls( $widget ) {
+
+		$config = $this->get_config();
+
+		if ( empty( $config ) ) {
+			return;
+		}
+
+		$config  = array_keys( $config );
+		$options = array_combine( $config, $config );
+
+		$widget->add_control(
+			'jet_calc_cbs',
+			array(
+				'label'       => esc_html__( 'Calculated callbacks', 'jet-engine' ),
+				'type'        => \Elementor\Controls_Manager::SELECT,
+				'label_block' => true,
+				'description' => esc_html__( 'Select calculated callback to apply', 'jet-engine' ),
+				'default'     => '',
+				'options'     => $options,
+				'condition'   => array(
+					'dynamic_field_filter' => 'yes',
+					'filter_callback'      => array( 'jet_engine_calculated_field' ),
+				),
+			)
+		);
+
+		$widget->add_control(
+			'jet_calc_cb_args',
+			array(
+				'label'       => esc_html__( 'Additional arguments', 'jet-engine' ),
+				'type'        => \Elementor\Controls_Manager::TEXT,
+				'label_block' => true,
+				'description' => esc_html__( 'Pass any addtional arguments for calculated callback. For example increase/dercrease percentage, additional meta field key for default callbacks. Note: for sum_fields, multiply_fields and fields_diff callbacks you can pass multiple additional fields by separating fields name with comma - field_1, field_2, field_3', 'jet-engine' ),
+				'default'     => '',
+				'condition'   => array(
+					'dynamic_field_filter' => 'yes',
+					'filter_callback'      => array( 'jet_engine_calculated_field' ),
+				),
+			)
+		);
+
 	}
 
 	public function add_calc_callback( $callbacks ) {
@@ -30,7 +88,7 @@ class Jet_Engine_Calculated_Callback_Addon {
 		return $callbacks;
 	}
 
-	public function calculated_field( $field_value, $calc_key = null ) {
+	public function calculated_field( $field_value = null, $calc_key = null, $args = null ) {
 
 		if ( ! $calc_key ) {
 			return $field_value;
@@ -42,27 +100,118 @@ class Jet_Engine_Calculated_Callback_Addon {
 			return $field_value;
 		}
 
-		return $field_1_value * get_post_meta( get_the_ID(), 'field_2' );
+		$callback = ! empty( $config[ $calc_key ] ) ? $config[ $calc_key ] : false;
+
+		if ( ! $callback || ! is_callable( $callback ) ) {
+			return $field_value;
+		} else {
+			return call_user_func( $callback, $field_value, $args );
+		}
 
 	}
 
 	public function get_config() {
-		return apply_filters( 'jet-engine-calculated-callback/config', array() );
+		return apply_filters( 'jet-engine-calculated-callback/config', array(
+			'increase_value_by_percentage' => function( $field_value, $percent = 0 ) {
+				if ( ! $percent ) {
+					return 'Please set percentage value to calculate';
+				}
+				return $field_value + $field_value * $percent / 100;
+			},
+			'decrease_value_by_percentage' => function( $field_value, $percent = 0 ) {
+				if ( ! $percent ) {
+					return 'Please set percentage value to calculate';
+				}
+				return $field_value - $field_value * $percent / 100;
+			},
+			'sum_fields' => function( $field_value, $fields ) {
+
+				if ( empty( $fields ) ) {
+					return 'Please set additional fields names to calculate';
+				}
+
+				$res    = absint( $field_value );
+				$fields = explode( ',', str_replace( ' ', '', $fields ) );
+
+				foreach ( $fields as $field ) {
+					$res += absint( jet_engine()->listings->data->get_meta( $field ) );
+				}
+
+				return $res;
+
+			},
+			'fields_diff' => function( $field_value, $fields ) {
+
+				if ( empty( $fields ) ) {
+					return 'Please set additional fields names to calculate';
+				}
+
+				$res    = absint( $field_value );
+				$fields = explode( ',', str_replace( ' ', '', $fields ) );
+
+				foreach ( $fields as $field ) {
+					$res = $res - absint( jet_engine()->listings->data->get_meta( $field ) );
+				}
+
+				return $res;
+
+			},
+			'mupltiple_fields' => function( $field_value, $fields ) {
+
+				if ( empty( $fields ) ) {
+					return 'Please set additional fields names to calculate';
+				}
+
+				$res    = absint( $field_value );
+				$fields = explode( ',', str_replace( ' ', '', $fields ) );
+
+				foreach ( $fields as $field ) {
+					$res = $res * absint( jet_engine()->listings->data->get_meta( $field ) );
+				}
+
+				return $res;
+
+			},
+		) );
 	}
 
 	public function add_field_to_args( $args, $callback, $settings = array() ) {
 
 		if ( 'jet_engine_calculated_field' === $callback ) {
-			$args[] = isset( $settings['dynamic_field_post_meta_custom'] ) ? $settings['dynamic_field_post_meta_custom'] : false;
+			$args[] = isset( $settings['jet_calc_cbs'] ) ? $settings['jet_calc_cbs'] : false;
+			$args[] = isset( $settings['jet_calc_cb_args'] ) ? $settings['jet_calc_cb_args'] : false;
 		}
 
 		return $args;
 	}
 
+	/**
+	 * Instance.
+	 *
+	 * Ensures only one instance of the plugin class is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 *
+	 * @return Plugin An instance of the class.
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$instance ) ) {
+
+			self::$instance = new self();
+
+		}
+
+		return self::$instance;
+
+	}
+
 }
 
-$jet_engine_calculated = new Jet_Engine_Calculated_Callback_Addon( $field_value, $calc_key );
+Jet_Engine_Calculated_Callback_Addon::instance();
 
-function jet_engine_calculated_field() {
-	return $jet_engine_calculated->calculated_field( $field_value, $calc_key );
+function jet_engine_calculated_field( $field_value = null, $calc_key = null, $args = null ) {
+	return Jet_Engine_Calculated_Callback_Addon::instance()->calculated_field( $field_value, $calc_key, $args );
 }
